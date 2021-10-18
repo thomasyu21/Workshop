be3b8cc3c139753bab20ea6506e38c59238bf547
thomasyu21 <thomasyu085@gmail.com>
2021-10-17 21:08:09 -0400

    Fixed request to use request.method. Added comments.

--- 14_form/app.py
+++ 14_form/app.py
@@ -23,42 +23,42 @@ Execute your tests. Process results.
 PROTIP: Insert your own in-line comments wherever they will help your future self and/or current teammates understand what is going on.
 '''
 
-teamBerd = "Team Berd: Austin Ngan, Thomas Yu, Mark Zhu"
-req = "<Request 'http://127.0.0.1:5000/' [GET]>"
-greet = "Hullo humon, Berd appreciates your visit. Enjoy your stay."
+teamBerd = "Team Berd: Austin Ngan, Thomas Yu, Mark Zhu" #TNPG + roster for both landing and response pages
+greet = "Hullo humon, Berd appreciates your visit. Enjoy your stay." #Greeting to be displayed on the response page
 
 2021-10-14
 @app.route("/") #, methods=['GET', 'POST'])
 def disp_loginpage():
-    print("\n\n\n")
-    print("***DIAG: this Flask obj ***")
-    print(app)
-    print("***DIAG: request obj ***")
-    print(request)
-    print("***DIAG: request.args ***")
-    print(request.args)
+    '''For the landing page where the user will login with a username'''
+    #print("\n\n\n")
+    #print("***DIAG: this Flask obj ***")
+    #print(app)
+    #print("***DIAG: request obj ***")
+    #print(request)
+    #print("***DIAG: request.args ***") #Prints a dictionary that is empty
+    #print(request.args)
     #print("***DIAG: request.args['username']  ***")
     #print(request.args['username'])
-    print("***DIAG: request.headers ***")
-    print(request.headers)
-    return render_template( 'login.html' , heading = teamBerd)
+    #print("***DIAG: request.headers ***")
+    #print(request.headers)
+    return render_template( 'login.html' , heading = teamBerd) #Only thing that is added to login page is the heading
 
 
 @app.route("/auth") # , methods=['GET', 'POST'])
 def authenticate():
-    print("\n\n\n")
-    print("***DIAG: this Flask obj ***")
-    print(app)
-    print("***DIAG: request obj ***")
-    print(request)
-    print("***DIAG: request.args ***")
-    print(request.args)
+    '''The response page that will display a greeting, the username, and the request method used'''
+    #print("\n\n\n")
+    #print("***DIAG: this Flask obj ***")
+    #print(app)
+    #print("***DIAG: request obj ***")
+    #print(request)
+    #print("***DIAG: request.args ***")#Prints a dictionary that now has the username as well as submit.
+    #print(request.args)
     #print("***DIAG: request.args['username']  ***")
     #print(request.args['username'])
-    print("***DIAG: request.headers ***")
-    print(request.headers)
-    return render_template('response.html', heading = teamBerd, greeting = greet, username = (request.args).get("username"), request = req)  #response to a form submission
-
+    #print("***DIAG: request.headers ***")
+    #print(request.headers)
+    return render_template('response.html', heading = teamBerd, greeting = greet, username = request.args['username'], request = request.method)  #uses response template to create the webpage
 
 
 if __name__ == "__main__": #false if this file imported as module
