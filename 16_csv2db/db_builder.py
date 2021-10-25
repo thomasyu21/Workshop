8dab8a08ade7c52b4e839e0989b70f8e6a939e34
thomasyu21 <thomasyu085@gmail.com>
2021-10-24 20:46:10 -0400

    Updated db_builder with cleaned up code. Updated notes.txt

--- 16_csv2db/db_builder.py
+++ 16_csv2db/db_builder.py
@@ -1,7 +1,7 @@
 #Team Berd: Austin Ngan (Gerald), Thomas Yu (Perry), Mark Zhu (Bob the Third Jr)
 #SoftDev
 #K16: All About Database
-#2021-10-21
+#2021-10-24
 
 import sqlite3   #enable control of an sqlite database
 import csv       #facilitate CSV I/O
@@ -14,12 +14,22 @@ c = db.cursor()               #facilitate db ops -- you will use cursor to trigg
 
 #==========================================================
 
+'''adds name, age, and id to the corresponding parts of the roster table'''
+c.execute("CREATE TABLE IF NOT EXISTS roster (name TEXT, age INTEGER, id INTEGER)") #creates the table
+with open("students.csv", newline='') as csvfile: #reads in the csv file
+    studentReader = csv.DictReader(csvfile)
+    for row in studentReader:
+        c.execute("INSERT INTO roster VALUES (?, ?, ?)", (f"{row['name']}", row['age'], row['id'])) #adds each row of the csv file into the db
 
-# < < < INSERT YOUR TEAM'S POPULATE-THE-DB CODE HERE > > >
+'''adds code, mark, and id to the corresponding parts of the courses table'''
+c.execute("CREATE TABLE IF NOT EXISTS courses (code TEXT, mark INTEGER, id INTEGER)") #creates table
+with open("courses.csv", newline='') as csvfile: #reads in csv file
+    coursesReader = csv.DictReader(csvfile)
+    for row in coursesReader:
+        c.execute("INSERT INTO courses VALUES (?, ?, ?)", (f"{row['code']}", row['mark'], row['id'])) #adds each row of the csv file into the db
 
-
-command = ""          # test SQL stmt in sqlite3 shell, save as string
-c.execute(command)    # run SQL statement
+#command = ""          # test SQL stmt in sqlite3 shell, save as string
+#c.execute(command)    # run SQL statement
 
 #==========================================================
 
