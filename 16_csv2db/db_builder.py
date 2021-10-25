190db66ed5f5226b7a77a4e4bf2550aef7125c21
thomasyu21 <thomasyu085@gmail.com>
2021-10-24 21:11:46 -0400

    Updated db_builder.py to not add data multiple times

--- 16_csv2db/db_builder.py
+++ 16_csv2db/db_builder.py
@@ -16,17 +16,21 @@ c = db.cursor()               #facilitate db ops -- you will use cursor to trigg
 
 '''adds name, age, and id to the corresponding parts of the roster table'''
 c.execute("CREATE TABLE IF NOT EXISTS roster (name TEXT, age INTEGER, id INTEGER)") #creates the table
-with open("students.csv", newline='') as csvfile: #reads in the csv file
-    studentReader = csv.DictReader(csvfile)
-    for row in studentReader:
-        c.execute("INSERT INTO roster VALUES (?, ?, ?)", (f"{row['name']}", row['age'], row['id'])) #adds each row of the csv file into the db
+c.execute("SELECT * FROM roster") #gets table roster from db file
+if len(c.fetchall()) == 0: #if length of list is 0, add data
+    with open("students.csv", newline='') as csvfile: #reads in the csv file
+        studentReader = csv.DictReader(csvfile)
+        for row in studentReader:
+            c.execute("INSERT INTO roster VALUES (?, ?, ?)", (f"{row['name']}", row['age'], row['id'])) #adds each row of the csv file into the db
 
 '''adds code, mark, and id to the corresponding parts of the courses table'''
 c.execute("CREATE TABLE IF NOT EXISTS courses (code TEXT, mark INTEGER, id INTEGER)") #creates table
-with open("courses.csv", newline='') as csvfile: #reads in csv file
-    coursesReader = csv.DictReader(csvfile)
-    for row in coursesReader:
-        c.execute("INSERT INTO courses VALUES (?, ?, ?)", (f"{row['code']}", row['mark'], row['id'])) #adds each row of the csv file into the db
+c.execute("SELECT * FROM courses")#gets table roster from db file
+if len(c.fetchall()) == 0: #if length of list is 0, add data
+    with open("courses.csv", newline='') as csvfile: #reads in csv file
+        coursesReader = csv.DictReader(csvfile)
+        for row in coursesReader:
+            c.execute("INSERT INTO courses VALUES (?, ?, ?)", (f"{row['code']}", row['mark'], row['id'])) #adds each row of the csv file into the db
 
 #command = ""          # test SQL stmt in sqlite3 shell, save as string
 #c.execute(command)    # run SQL statement
